71886909e04c16c51ed03d9ce8d970078273a0af
David Harrison <16119203+mr-mocap@users.noreply.github.com>
2024-09-04 20:01:18 -0400

    Add templated exp() function

--- MathLib/include/math/Exponential.hpp
+++ MathLib/include/math/Exponential.hpp
@@ -1,3 +1,58 @@
 #pragma once
 
 
+/** @file
+ *  
+ *  @hideincludegraph
+ */
+
+/** @addtogroup Exponential
+ * 
+ *  @{
+ *  
+ *  Create the exponential form of the input
+ *  
+ */
+
+/** @name General Template Case
+ *  
+ *  @{
+ */
+
+/**  Computes the exponential of the input
+ * 
+ *   @note This will just call @c input.exp()
+ */
+template<class T>
+constexpr T exp(T input)
+{
+    return input.exp();
+}
+/// @}
+
+/** @name Specializations
+ * 
+ *  These are the basic default template specializations for @c exp()
+ *  from above
+ */
+/// @{
+template<>
+constexpr float exp<float>(float input)
+{
+    return std::exp(input);
+}
+
+template<>
+constexpr double exp<double>(double input)
+{
+    return std::exp(input);
+}
+
+template<>
+constexpr long double exp<long double>(long double input)
+{
+    return std::exp(input);
+}
+/// @}
+
+/// @}
