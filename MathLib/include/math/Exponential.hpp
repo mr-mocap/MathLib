367778bbdded0ece4bcd16183e8759574be42630
David Harrison <16119203+mr-mocap@users.noreply.github.com>
2024-09-13 12:14:30 -0400

    Remove redefined exp() & log() global

--- MathLib/include/math/Exponential.hpp
+++ MathLib/include/math/Exponential.hpp
@@ -29,31 +29,6 @@ T exp(T input)
     return input.exp();
 }
 /// @}  {General Template Case}
-
-/** @name Specializations
- * 
- *  These are the basic default template specializations for @c exp()
- *  from above
- */
-/// @{
-template<>
-float exp<float>(float input)
-{
-    return std::exp(input);
-}
-
-template<>
-double exp<double>(double input)
-{
-    return std::exp(input);
-}
-
-template<>
-long double exp<long double>(long double input)
-{
-    return std::exp(input);
-}
-/// @}  {Specializations}
 /// @}  {Exponential}
 
 
@@ -80,29 +55,4 @@ T log(T input)
     return input.log();
 }
 /// @}  {General Template Case}
-
-/** @name Specializations
- * 
- *  These are the basic default template specializations for @c log()
- *  from above
- */
-/// @{
-template<>
-float log<float>(float input)
-{
-    return std::log(input);
-}
-
-template<>
-double log<double>(double input)
-{
-    return std::log(input);
-}
-
-template<>
-long double log<long double>(long double input)
-{
-    return std::log(input);
-}
-/// @}  {Specializations}
 /// @}  {Log}
