c9af5013cfca135223228e8df18d3738eb827cd7
Mr Mocap <16119203+mr-mocap@users.noreply.github.com>
2025-01-12 14:03:21 -0500

    Remove our version of clamp

--- MathLib/include/math/Functions.hpp
+++ MathLib/include/math/Functions.hpp
@@ -7,6 +7,7 @@
 #include "math/Trigonometric.hpp"
 #include "math/Checks.hpp"
 #include "math/Combinatorics.hpp"
+#include <algorithm>
 
 /** @file
  *  
@@ -40,18 +41,15 @@ constexpr inline Type remap(const Type input_value, const Type orig_range_min, c
 }
 
 template <class Type>
-constexpr inline Type clamp(const Type input_value, const Type lower_bound = 0, const Type upper_bound = 1)
+constexpr inline const Type &saturate(const Type &value, const Type &lower_bound, const Type &upper_bound)
 {
-    if constexpr (std::is_floating_point<Type>::value)
-        return std::fmin(std::fmax(lower_bound, input_value), upper_bound);
-    else
-        return std::min(std::max(lower_bound, input_value), upper_bound);
+    return std::clamp( value, lower_bound, upper_bound );
 }
 
 constexpr inline float smoothstep(float input_value, const float left_edge = 0.0f, const float right_edge = 1.0f)
 {
     // Scale and clamp to 0..1 range
-    input_value = clamp<float>((input_value - left_edge) / (right_edge - left_edge));
+    input_value = std::clamp<float>((input_value - left_edge) / (right_edge - left_edge), 0.0f, 1.0f);
 
     return input_value * input_value * (3.0f - 2.0f * input_value);
 }
@@ -61,7 +59,7 @@ template <class Type>
 constexpr inline Type smootherstep(Type input_value, const Type left_edge, const Type right_edge)
 {
     // Scale and clamp to 0..1 range
-    input_value = clamp((input_value - left_edge) / (right_edge - left_edge));
+    input_value = std::clamp((input_value - left_edge) / (right_edge - left_edge), Type{0}, Type{1});
     if constexpr (std::is_same<Type, float>::value)
         return input_value * input_value * input_value * (input_value * (6.0f * input_value - 15.0f) + 10.0f);
     else
@@ -72,7 +70,7 @@ template <class Type = float>
 constexpr inline Type smoothstep_generalized(Type input_value, const Type left_edge = 0, const Type right_edge = 1, const int Order = 1)
 {
     // Scale and clamp to 0..1 range
-    input_value = clamp<Type>((input_value - left_edge) / (right_edge - left_edge));
+    input_value = std::clamp<Type>((input_value - left_edge) / (right_edge - left_edge), Type{0}, Type{1});
     
     // Order of 0 mathematically works out to just the clamp
     if (Order == 0)
@@ -93,9 +91,9 @@ constexpr inline Type smoothstep_generalized(Type input_value, const Type left_e
 }
 
 template <class Type = float>
-inline Type inverse_smoothstep(const Type input_value)
+constexpr inline Type inverse_smoothstep(const Type input_value)
 {
-    return Type{0.5} - std::sin( std::asin(Type{1.0} - Type{2.0} * clamp<Type>(input_value)) / Type{3.0} );
+    return Type{0.5} - std::sin( std::asin(Type{1.0} - Type{2.0} * std::clamp<Type>(input_value, Type{0}, Type{1})) / Type{3.0} );
 }
 
 }
