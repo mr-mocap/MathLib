e8a94d65213de0b8493770e0d27a5a6d8eb3ac8a
Mr Mocap <16119203+mr-mocap@users.noreply.github.com>
2025-01-31 17:09:34 -0500

    Refactor: Change templated func to normal funcs

--- MathLib/include/math/ApproximatelyEqualTo.hpp
+++ MathLib/include/math/ApproximatelyEqualTo.hpp
@@ -24,8 +24,17 @@ namespace Math
  * 
  *  @return @c true if the two are equal within @c tolerance , @c false otherwise
  */
-template <std::floating_point Type>
-inline bool approximately_equal_to(Type input, Type near_to, float tolerance = 0.0002f)
+inline bool approximately_equal_to(float input, float near_to, float tolerance = 0.0002f)
+{
+    return std::abs(near_to - input) <= tolerance;
+}
+
+inline bool approximately_equal_to(double input, double near_to, float tolerance = 0.0002f)
+{
+    return std::abs(near_to - input) <= tolerance;
+}
+
+inline bool approximately_equal_to(long double input, long double near_to, float tolerance = 0.0002f)
 {
     return std::abs(near_to - input) <= tolerance;
 }
