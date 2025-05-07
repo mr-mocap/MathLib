0859c81b8459e7117730bad76862e29448f05a74
Mr Mocap <16119203+mr-mocap@users.noreply.github.com>
2025-05-07 12:12:29 -0400

    Add a color gradient generation function

---
+++ MathLib/include/color/Palette.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <math/Vector3D.hpp>
+#include <cstdlib>
+#include <numbers>
+
+namespace Color
+{
+
+/** Generate a continuous color gradient
+ *   
+ *   @param t 
+ *   @param a 
+ *   @param b 
+ *   @param c 
+ *   @param d 
+ * 
+ *   @return vec3 
+ * 
+ *   @note Borrowed from: https://iquilezles.org/articles/palettes/
+ */
+Vector3Df palette(float t, Vector3Df a, Vector3Df b, Vector3Df c, Vector3Df d)
+{
+    return a + b * std::cos( 2 * std::pi_v<float> * (c * t + d) );
+}
+
+}
