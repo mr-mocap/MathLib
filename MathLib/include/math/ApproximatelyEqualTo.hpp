0d382e9445ae33b098f669bf0359ee76d1cd1713
David Harrison <16119203+mr-mocap@users.noreply.github.com>
2025-12-30 07:56:47 -0500

    Add three way compare with a tolerance

--- MathLib/include/math/ApproximatelyEqualTo.hpp
+++ MathLib/include/math/ApproximatelyEqualTo.hpp
@@ -2,7 +2,6 @@
 
 #include <cmath>
 
-
 /** @file
  *  
  *  @hideincludegraph
@@ -36,6 +35,45 @@ inline bool approximately_equal_to(double input, double near_to, double toleranc
 inline bool approximately_equal_to(long double input, long double near_to, long double tolerance = 0.0002l)
 {
     return std::abs(near_to - input) <= tolerance;
+}
+/// @}
+
+/** @addtogroup Comparison
+ * 
+ *  @{
+ *  
+ *  Compare two values for equality, less-than, or greater-than with a tolerance
+ *  
+ *  @param left      The left value to compare
+ *  @param right     The right value to compare
+ *  @param tolerance The minimum value for being considered equal
+ * 
+ *  @return @c 0 if the two are equal within @c tolerance , @c -1 if @c left < @c right , @c 1 if @c left > @c right
+ * 
+ *  @note This is similar to the three-way comparison operator (<=>), but with a tolerance for equality
+ */
+inline int three_way_compare(float left, float right, float tolerance = 0.0002f)
+{
+    if ( approximately_equal_to(left, right, tolerance) )
+        return 0;
+
+    return (left < right) ? -1 : 1;
+}
+
+inline int three_way_compare(double left, double right, double tolerance = 0.0002)
+{
+    if ( approximately_equal_to(left, right, tolerance) )
+        return 0;
+
+    return (left < right) ? -1 : 1;
+}
+
+inline int three_way_compare(long double left, long double right, long double tolerance = 0.0002l)
+{
+    if ( approximately_equal_to(left, right, tolerance) )
+        return 0;
+
+    return (left < right) ? -1 : 1;
 }
 /// @}
 
