f04d4943c18c3e71f708eebd1937c07f793f078c
Mr Mocap <16119203+mr-mocap@users.noreply.github.com>
2026-06-19 15:50:04 -0400

    Add documentation for a Concept

--- MathLib/lib/include/Math/Concepts.hpp
+++ MathLib/lib/include/Math/Concepts.hpp
@@ -5,6 +5,11 @@
 namespace Concept
 {
 
+/** Determines whether two types are the same after stripping const & volatile from them both
+ *   
+ *  @tparam T The main type you are comparing
+ *  @tparam O The other type to compare against
+ */
 template <typename T, typename O>
 concept SameUnqualified = std::is_same_v< std::remove_cv_t<T>, std::remove_cv_t<O> >;
 
